111634953d25cf77555cdfb752a421f1c7bef3bd
Stavros Skourtis <stavros.skourtis@gmail.com>
2015-03-08 16:51:15 +0200

    Version 1.1
    This is a much better version.
    All seem to work fine, some more test and a few commits and this will be ready

--- src/TestXML.java
+++ src/TestXML.java
@@ -1,24 +1,30 @@
 import java.io.IOException;
+import java.util.LinkedList;
 
 /**
  *
  * @author Stavros Skourtis
  */
 public class TestXML {
-        
-    public static void main(String args[]) throws IOException{
-        
-        XMLNode node = new XMLNode("message");
-        node.addAttribute("from", "username");
-        node.addAttribute("to", "user2");
-        node.setInnerText("hello, how are you?");
-        
-        XMLFile file = new XMLFile();
-        
-        file.addNode(node);
-        
-        
-        System.out.println(file);
-        
-    }
+
+	public static void main(String args[]) throws IOException {
+		String xml = "<root>"
+				+ " <message type='public' from='Name Surname' to='John Doe' >"
+				+ "Hello Friend"
+				+ "</message>"
+				+ "<info type='this is an xml document' />"
+				+ "</root>";
+		
+		XmlParser parser = new XmlParser(xml);
+		XmlElementNode root = parser.parse();
+		
+		LinkedList<XmlNode> messages = root.getSubElements("message");
+		System.out.println(messages.size());
+		System.out.println( ((XmlElementNode)messages.get(0)).getAttribute("to") );
+		System.out.println( ((XmlElementNode)messages.get(0)).getText() );
+		
+		LinkedList<XmlNode> info = root.getSubElements("info");
+		System.out.println( ((XmlElementNode)info.get(0)).getAttribute("type") );
+	}
+
 }
